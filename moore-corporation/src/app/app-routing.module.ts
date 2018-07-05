c24107b2769a756c332fc12e732a1ea34b51d70e
Joseph Moore <josephmoore@Josephs-MacBook-Pro.local>
2018-07-04 23:38:46 -0700

    first push to production :D

--- moore-corporation/src/app/app-routing.module.ts
+++ moore-corporation/src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { CoverComponent } from './cover/cover.component';
+
+const routes: Routes = [
+  { path: '',
+    redirectTo: '/cover',
+    pathMatch: 'full'
+  },
+  { path: 'cover', component: CoverComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'contact', component: ContactComponent }
+];
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forRoot(routes)
   ],
+  exports: [ RouterModule ],
   declarations: []
 })
 export class AppRoutingModule { }
--- moore-corporation/src/app/app.module.ts
+++ moore-corporation/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './/app-routing.module';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { CoverComponent } from './cover/cover.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContactComponent,
+    CoverComponent,
   ],
   imports: [
     BrowserModule,
